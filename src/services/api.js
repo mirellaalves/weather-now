124fb21a11ca9c9e8f3a3693ed80da3f3ce0b243
mirellaalves <mirella.alves@gmail.com>
2021-03-15 15:05:45 -0300

    change api key

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.openweathermap.org/data/2.5/weather?q=Nuuk&appid=caw'
+  baseURL: 'https://api.openweathermap.org/data/2.5/weather?q=Nuuk&appid=cc07f1fe0b9145888216df79e5733261'
 });
 
 export default api;
